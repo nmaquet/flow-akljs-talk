351a6d27dd1244d4df0c1b5cde0bc0979594fb07
Nicolas Maquet <nicolas@movio.co>
2016-11-08 17:32:10 +1300

    --wip--

--- code/7__optional_and_maybe.js
+++ code/7__optional_and_maybe.js
@@ -5,11 +5,20 @@
  { foo? : T } is optionally T, meaning T | void
  { foo : ?T } is maybe T, meaning T | null | void
 
+  No sugar for `T | null`, but easy enough to type anyway
+
  */
 
-type Smurf = {
-    name: string,
-    age?: number,
-    isAwake?: boolean
+type Smurf = { name: string, age: ?number };
+
+const papa: Smurf = { name: "Papa Smurf", age: 300 };
+const smurfette: Smurf = { name: "Smurfette", age: 200 };
+
+function toString(smurf: Smurf, suffix: string = "") {
+    if (smurf.age == null) {
+        return `${smurf.name} is immortal ${suffix}`;
+    }
+    return `${smurf.name} is ${smurf.age} years old ${suffix}`;
 }
 
+const x: number | null = 42;
